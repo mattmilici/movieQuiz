9aee34f245be86d5d8c128d2f8a87475d9413056
Matthew Milici <milici.m915@gmail.com>
2020-08-08 16:09:14 -0700

    test

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    purge: [],
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+        },
+        colors: {
+            green1: "#006a71",
+            tan1: "#ffffdd",
+            baby1: "#cbeaed",
+            lime1: "#d3de32",
+        },
+    },
+    variants: {},
+    plugins: [],
+};
