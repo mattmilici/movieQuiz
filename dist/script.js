6169e166c28cc4593cfa48bf94c48c9bc6a19c97
Matthew Milici <milici.m915@gmail.com>
2020-08-09 08:01:48 -0700

    updates to css

---
+++ dist/script.js
@@ -0,0 +1,17 @@
+$("#hideBtn").on("click", hideFunction);
+
+$("#cardView").hide();
+
+function hideFunction() {
+    var buttonStatus = $("#hideBtn").attr("id");
+
+    if (buttonStatus === "hideBtn") {
+        $("#textChat").hide();
+        $("#hideBtn").attr("id", "showBtn");
+        $("#cardView").show();
+    } else {
+        $("#textChat").show();
+        $("#showBtn").attr("id", "hideBtn");
+        $("#cardView").hide();
+    }
+}
