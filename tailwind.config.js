23662a96b62c8b57632ca0f325dc19d7e9a2c045
Matthew Milici <milici.m915@gmail.com>
2020-08-08 18:06:11 -0700

    upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
             "screen/2": "50vh",
             "screen/3": "calc(100vh * .6)",
             "screen/4": "calc(100vh / 4)",
-            "screen/5": "calc(100vh / 5)",
+            "screen/5": "calc(100vh * .1)",
             "screen/1": "100vh",
         }),
     },
